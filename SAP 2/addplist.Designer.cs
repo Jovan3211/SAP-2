87957ec53243692da26adfdb2b2917b3a219c0d3
Jovan3211 <pady.soft@gmail.com>
2015-11-22 13:29:20 +0100

    0.4.0
    DODACI
    -Dodata funkcionalnost forme 'addplist' za kreiranje plejlisti.
    -Dodati messagebox-ovi koji daju feedback i greske.
    -Mogu da se dodaju i sklanjaju pesme u textbox.
    -Moze da se kreira '.m3u' playlist fajl.
    -Prikaze se slika kad se drag and dropuje
    
    POPRAVKE
    -Popravljeno prikazivanje fajl path-a u textbox i u listu pesama.
    -Povecana forma 'addplaylist'.
    
    PROBLEMI
    -Svi problemi kao i u 0.2.0
    -Kada se drag and dropuje preko slike, ta slika se ugasi.

--- SAP 2/addplist.Designer.cs
+++ SAP 2/addplist.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addplist));
             this.textBox_playlistName = new System.Windows.Forms.TextBox();
             this.label_playListName = new System.Windows.Forms.Label();
             this.label_saveLocation = new System.Windows.Forms.Label();
@@ -41,13 +42,15 @@
             this.button_addSongDialog = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button_createPlaylist = new System.Windows.Forms.Button();
+            this.pictureBox_dragDrop = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_dragDrop)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox_playlistName
             // 
             this.textBox_playlistName.Location = new System.Drawing.Point(100, 12);
             this.textBox_playlistName.Name = "textBox_playlistName";
-            this.textBox_playlistName.Size = new System.Drawing.Size(301, 20);
+            this.textBox_playlistName.Size = new System.Drawing.Size(440, 20);
             this.textBox_playlistName.TabIndex = 0;
             // 
             // label_playListName
@@ -72,12 +75,12 @@
             // 
             this.textBox_saveLocation.Location = new System.Drawing.Point(100, 38);
             this.textBox_saveLocation.Name = "textBox_saveLocation";
-            this.textBox_saveLocation.Size = new System.Drawing.Size(270, 20);
+            this.textBox_saveLocation.Size = new System.Drawing.Size(409, 20);
             this.textBox_saveLocation.TabIndex = 3;
             // 
             // button_saveLocationDialog
             // 
-            this.button_saveLocationDialog.Location = new System.Drawing.Point(376, 37);
+            this.button_saveLocationDialog.Location = new System.Drawing.Point(515, 38);
             this.button_saveLocationDialog.Name = "button_saveLocationDialog";
             this.button_saveLocationDialog.Size = new System.Drawing.Size(25, 20);
             this.button_saveLocationDialog.TabIndex = 4;
@@ -91,8 +94,10 @@
             this.listBox.FormattingEnabled = true;
             this.listBox.Location = new System.Drawing.Point(100, 103);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(301, 147);
+            this.listBox.Size = new System.Drawing.Size(440, 199);
             this.listBox.TabIndex = 5;
+            this.listBox.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox_DragOver);
+            this.listBox.DragLeave += new System.EventHandler(this.listBox_DragLeave);
             // 
             // button_add
             // 
@@ -116,15 +121,18 @@
             // 
             // textBox_addSong
             // 
+            this.textBox_addSong.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_addSong.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox_addSong.Location = new System.Drawing.Point(100, 64);
             this.textBox_addSong.Name = "textBox_addSong";
-            this.textBox_addSong.Size = new System.Drawing.Size(270, 20);
+            this.textBox_addSong.Size = new System.Drawing.Size(409, 20);
             this.textBox_addSong.TabIndex = 8;
             this.textBox_addSong.Text = "Select a song to add.";
+            this.textBox_addSong.Click += new System.EventHandler(this.textBox_addSong_Click);
             // 
             // button_addSongDialog
             // 
-            this.button_addSongDialog.Location = new System.Drawing.Point(376, 63);
+            this.button_addSongDialog.Location = new System.Drawing.Point(515, 64);
             this.button_addSongDialog.Name = "button_addSongDialog";
             this.button_addSongDialog.Size = new System.Drawing.Size(25, 20);
             this.button_addSongDialog.TabIndex = 9;
@@ -138,18 +146,31 @@
             // 
             // button_createPlaylist
             // 
-            this.button_createPlaylist.Location = new System.Drawing.Point(12, 209);
+            this.button_createPlaylist.Location = new System.Drawing.Point(12, 261);
             this.button_createPlaylist.Name = "button_createPlaylist";
             this.button_createPlaylist.Size = new System.Drawing.Size(75, 41);
             this.button_createPlaylist.TabIndex = 10;
             this.button_createPlaylist.Text = "Create playlist";
             this.button_createPlaylist.UseVisualStyleBackColor = true;
+            this.button_createPlaylist.Click += new System.EventHandler(this.button_createPlaylist_Click);
+            // 
+            // pictureBox_dragDrop
+            // 
+            this.pictureBox_dragDrop.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox_dragDrop.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_dragDrop.Image")));
+            this.pictureBox_dragDrop.Location = new System.Drawing.Point(261, 137);
+            this.pictureBox_dragDrop.Name = "pictureBox_dragDrop";
+            this.pictureBox_dragDrop.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox_dragDrop.TabIndex = 11;
+            this.pictureBox_dragDrop.TabStop = false;
+            this.pictureBox_dragDrop.Visible = false;
             // 
             // addplist
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 262);
+            this.ClientSize = new System.Drawing.Size(552, 312);
+            this.Controls.Add(this.pictureBox_dragDrop);
             this.Controls.Add(this.button_createPlaylist);
             this.Controls.Add(this.button_addSongDialog);
             this.Controls.Add(this.textBox_addSong);
@@ -162,7 +183,8 @@
             this.Controls.Add(this.label_playListName);
             this.Controls.Add(this.textBox_playlistName);
             this.Name = "addplist";
-            this.Text = "Add playlist";
+            this.Text = "Create playlist";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_dragDrop)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,5 +205,6 @@
         private System.Windows.Forms.Button button_addSongDialog;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button_createPlaylist;
+        private System.Windows.Forms.PictureBox pictureBox_dragDrop;
     }
 }
--- SAP 2/addplist.cs
+++ SAP 2/addplist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
@@ -9,8 +10,9 @@ namespace SAP_2
     {
         //kreiranje liste gde ce stojati pesme koje ce se ubaciti u fajl koji se cuva kad se klikne 'create playlist'
         List<string> playlistSongs = new List<string>();
-        //deklaracija file dialoga
+        //deklaracije
         OpenFileDialog open = new OpenFileDialog();
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
 
         public addplist()
         {
@@ -36,15 +38,22 @@ namespace SAP_2
 
             if (open.ShowDialog() == DialogResult.OK)
             {
-                textBox_addSong.Text = Path.GetFileName(open.FileName);
+                textBox_addSong.Text = open.FileName;
             }
         }
 
         //dodavanje pesme u textbox i listu plejliste
         private void button_add_Click(object sender, EventArgs e)
         {
-            listBox.Items.Add(textBox_addSong.Text);
-            playlistSongs.Add(open.FileName);
+            if (!File.Exists(open.FileName))
+            {
+                MessageBox.Show("Incorrect song file path!", "Error");
+            }
+            else
+            {
+                listBox.Items.Add(textBox_addSong.Text);
+                playlistSongs.Add(textBox_addSong.Text);
+            }
         }
 
         //uklanjanje pesme iz texboxa i liste
@@ -55,6 +64,64 @@ namespace SAP_2
                 listBox.Items.RemoveAt(listBox.SelectedIndex);
                 playlistSongs.Remove(Convert.ToString(listBox.SelectedIndex));
             }
+        }
+
+        //kreiranje plejliste
+        private void button_createPlaylist_Click(object sender, EventArgs e)
+        {
+            string playlistLocation = textBox_saveLocation.Text + "\\" + textBox_playlistName.Text + ".m3u";
+
+            if (!Directory.Exists(textBox_saveLocation.Text))
+            {
+                MessageBox.Show("Incorrect save location!", "Error");
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(playlistLocation))
+                {
+                    writer.WriteLine("#EXTM3U\n");
+
+                    foreach (string path in playlistSongs)
+                    {
+                        wplayer.URL = path;
+                        wplayer.controls.play();
+
+                        //uzimanje informacija o pesmi
+                        string duration = wplayer.currentMedia.durationString;
+                        string artist = wplayer.currentMedia.getItemInfo("Artist");
+                        string title = wplayer.currentMedia.getItemInfo("Title");
+
+                        wplayer.controls.stop();
+
+                        //pisanje u playlistu
+                        writer.WriteLine("#EXTINF:{0},{1} - {2}", duration, artist, title);
+                        writer.WriteLine("{0}\n", path);
+                    }
+                }
+
+                if (File.Exists(playlistLocation))
+                    MessageBox.Show("The playlist has been successfully created!", "Playlist created");
+                else
+                    MessageBox.Show("There has been an error during creation. The playlist does not exist!", "Error");
+            }
+        }
+
+        //italic text u textbox za dodavanje pesme
+        private void textBox_addSong_Click(object sender, EventArgs e)
+        {
+            textBox_addSong.Clear();
+            textBox_addSong.Font = new Font(textBox_addSong.Font, textBox_addSong.Font.Style & FontStyle.Regular);
+            textBox_addSong.ForeColor = Color.Black;
+        }
+
+        private void listBox_DragOver(object sender, DragEventArgs e)
+        {
+            pictureBox_dragDrop.Visible = true;
+        }
+
+        private void listBox_DragLeave(object sender, EventArgs e)
+        {
+            pictureBox_dragDrop.Visible = false;
         }
     }
 }
